17a27da76e0751fd8528e5fdf405095bfcc4fcc2
kazuki.nakamura <kajirikajiri@gmail.com>
2018-06-05 23:33:01 +0900

    add pom izonkankei

---
+++ src/model/versionCheck.java
@@ -0,0 +1,10 @@
+package model;
+
+public class versionCheck {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println(System.getProperty("java.version"));
+	}
+
+}
